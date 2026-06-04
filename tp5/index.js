2aead3009d6a55c7e0c61bb0e53989e2855a85b7
alumno <51085599@ivt.edu.ar>
2026-06-04 09:06:38 -0300

    4-6-26

---
+++ tp5/index.js
@@ -0,0 +1,27 @@
+//ejercicio 1a
+let nu1=20
+let nu2=20
+let suma = nu1 + nu2
+            //3a
+console.log("el resultado de la suma es " + suma);
+//ejercicio 1b 
+let nu3=50
+let nu4=30
+let resta= nu3-nu4
+            //3a
+console.log("el resultado de la resta es " + resta);
+//ejercicio 1c
+let nu5=2
+let nu6=6
+let mult= nu5*nu6
+console.log("el resultado de la multiplicación es " + mult);
+//elercicio 1d
+let nu7=8
+let nu8=2
+let div= nu7/nu8
+console.log("el resultado de la división es " + div);
+//ejercicio 2a
+alert(suma)
+alert (resta)
+//ejercicio 3b
+
