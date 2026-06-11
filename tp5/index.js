915d9617e29bfc262c223f17b2856ffc5a063c97
alumno <51085599@ivt.edu.ar>
2026-06-11 08:02:40 -0300

    11.6

--- tp5/index.js
+++ tp5/index.js
@@ -28,5 +28,28 @@ let nombreAlumno="Robert"
 //ejercicio 4b
 let apellidoAlumno="Irwin"
 //ejercicio 4c
-let edadAlumno=22
+let edadAlumno=22 
 //ejercicio 4d
+console.log("el alumno:"+nombreAlumno+apellidoAlumno+ " tiene"+ edadAlumno+"años");
+//ejercicio 5a
+let precio1=20
+let precio2=20
+let precio3=20
+//ejercicio 5b
+let total= precio1+precio2+precio3
+//ejercicio 5c
+console.log("el total a pagar es: " + total);
+//ejercicio 6a
+let comidaFavorita= "fideos con brocoli"
+let= colorFavorito="Azul oscuro"
+let numeroFavorito=11
+//ejercicio 6b
+console.log("mi comida favorita es "+ comidaFavorita+", me gusta el color"+ colorFavorito+ " y mi numero favorito es"+ numeroFavorito);
+//ejercicio 7a 
+let pais='Argentina'
+//ejercicio 7b
+console.log(pais);
+//ejercicio 7c
+pais='brasil'
+//ejercicio 7d
+console.log(pais);
